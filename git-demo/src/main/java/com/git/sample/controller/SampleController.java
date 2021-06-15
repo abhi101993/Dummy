fa283e8168b431324e318c6ab41ef57bf2658e72
Abhishek <abhi101993@gmail.com>
2021-06-16 02:26:23 +0530

    Add Repository class

--- git-demo/src/main/java/com/git/sample/controller/SampleController.java
+++ git-demo/src/main/java/com/git/sample/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.git.sample.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,13 @@ public class SampleController {
 
 @GetMapping("/hello")
 public String getMessage()
+{
+	return "Hello";
+}
+
+
+@PostMapping("/hello")
+public String getMess()
 {
 	return "Hello";
 }
---
+++ git-demo/src/main/java/com/git/sample/repository/SampleRepository.java
@@ -0,0 +1,5 @@
+package com.git.sample.repository;
+
+public class SampleRepository {
+
+}
